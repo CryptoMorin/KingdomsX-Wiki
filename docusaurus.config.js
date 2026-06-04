78b8f4dbd28f5d5a1203ec9f15411a3d705c547e
Crypto Morin <cryptomorin@gmail.com>
2026-06-04 13:11:46 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import remarkGFM from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
-import { rehypeForceH1Slug } from './plugins/rehyped_slug';
+import rehypeForceH1Slug from './plugins/rehyped_slug';
 
 import {themes} from 'prism-react-renderer';
 import prism from 'prismjs';
