fb4dc15a1b3409e52989bc7fa6909ecb4dc54603
Crypto Morin <cryptomorin@gmail.com>
2026-06-05 12:02:12 -0700

    Update index.js

--- src/theme/DocSidebar/index.js
+++ src/theme/DocSidebar/index.js
@@ -1,10 +1,12 @@
+import DocSidebar from '@theme-original/DocSidebar';
+
 export default function DocSidebar(props) {
   return (
     <aside className="custom-sidebar">
       <div className="my-header">My Custom Docs</div>
 
       {/* original sidebar content */}
-      <OriginalDocSidebar {...props} />
+      <DocSidebar {...props} />
 
       <div className="my-footer">Version 2.0</div>
     </aside>
