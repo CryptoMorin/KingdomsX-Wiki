3eb678ff982e3264ff9be64f75f3615e0821ff07
Crypto Morin <cryptomorin@gmail.com>
2026-06-05 02:30:56 -0700

    v99999999999999

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
+  console.log("redirect home")
   return <Redirect to="/docs/Home" />;
 }
