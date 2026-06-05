202f93870dbcd832c3c4bed7286099743448693a
Crypto Morin <cryptomorin@gmail.com>
2026-06-05 06:12:53 -0700

    v99999999999999999

--- plugins/remark-placeholders.js
+++ plugins/remark-placeholders.js
@@ -2,7 +2,12 @@ import { visit } from "unist-util-visit";
 
 export default function remarkPlaceholders() {
   return (tree) => {
-    visit(tree, "inlineCode", (node) => {
+    visit(tree, "inlineCode", (node, index, parent) => {
+      // Safety check: ensure it's not inside a code block context
+      // In MDAST, inlineCode parent should NOT be "code"
+      // if (!parent) return;
+      // if (parent.type === "code") return;
+
       const text = node.value.trim();
 
       if (
@@ -12,14 +17,16 @@ export default function remarkPlaceholders() {
       ) {
         const inner = text.slice(1, -1);
 
+        // Don't use <code> because Docusaurus will Prism-ify it.
+        // Causing "placeholder-text" to be replaced and a <pre> tag added.
         node.type = "html";
         node.value = `
-<code class="placeholder-text">
-  <span class="placeholder-enclosure">%</span>
-  <span class="placeholder-content">${inner}</span>
-  <span class="placeholder-enclosure">%</span>
-</code>
-`;
+<span class="placeholder-text">
+<span class="placeholder-enclosure">%</span>
+<span class="placeholder-content">${inner}</span>
+<span class="placeholder-enclosure">%</span>
+</span>
+`.trim();
       }
     });
   };
